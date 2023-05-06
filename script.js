0c00aa77f46658aba4a366e59c8a63130ed79968
Nikhil Sahu <heynikhil07@gmail.com>
2023-05-06 22:21:53 +0530

    Lecture: #2 Learned Destructing of Objects and Object parameters

--- script.js
+++ script.js
@@ -19,6 +19,25 @@ const restaurant = {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
 
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+
+    orderDelivery: function ({ starterIndex, mainIndex, address, time }) {
+        console.log(`Food ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered at ${address} at ${time}..!`);
+    }
+
 };
 
 // const arr = [1, 2, 3, 4];
@@ -62,3 +81,55 @@ const restaurant = {
 // const answers = ['Yes', 'Yes', 'Yes'];
 // const [a = 'No', b = 'No', c = 'No', d = 'No'] = answers; //setting default values to the variables
 // console.log(a, b, c, d);
+
+
+/////////////////////////////////////////
+//Destructing Objects
+
+// const { restaurantName, openingHours, categories } = restaurant;
+// console.log(restaurantName, openingHours, categories);
+
+// const { restaurantName: labelName, openingHours: hours, categories: foodCategories } = restaurant;
+// console.log(labelName, hours, foodCategories);
+
+//Mutating data of variables
+// let a = 111;
+// let b = 999;
+
+// const obj = {
+//     a: 11,
+//     b: 99,
+//     c: 77,
+// };
+
+// ({ a, b } = obj); //use of small brackets cause js thinks parenthesis as a expressions not declarations
+// console.log(a, b);
+
+//Default Values
+// const { complimentryDrink = [], starterMenu: starters = 4, mainMenu = [] } = restaurant;
+// console.log(complimentryDrink, starters, mainMenu);
+
+//Nested object destructing
+
+// const {
+//     openingHours: {
+//         fri: {
+//             open: o,
+//             close: c
+//         }
+//     }
+// } = restaurant;
+
+// console.log(o, c);
+
+//calling keys of objects in a functions parameters
+//so that sequence of parameters should not be remembered
+//we do so in bunch of parameters in js
+
+// restaurant.orderDelivery({
+//     time: '16:12',
+//     address: 'Via del sole, 21',
+//     mainIndex: 1,
+//     starterIndex: 3,
+// });
+
